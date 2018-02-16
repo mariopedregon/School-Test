74d355127fd906004b067b394916381eb317c05f
mario pedregon <mariopedregon@marios-MacBook-Air.local>
2018-02-15 17:33:01 -0700

    authenticaion basic user

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-Rails.application.routes.draw do
+lRails.application.routes.draw do
   devise_for :users
   resources :portfolios, except: [:show]
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
