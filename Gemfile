ee65b336b0af71944f1acedce20aa986408caebe
mario pedregon <mariopedregon@marios-MacBook-Air.local>
2018-04-09 21:48:47 -0600

    Dev tool initial commit

--- Gemfile
+++ Gemfile
@@ -52,4 +52,7 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'friendly_id', '~> 5.1.0'
 gem 'devise','~> 4.4','>=4.4.1'
+
 gem 'bootstrap', '~> 4.0.0'
+
+gem 'pedregon_view_tool', git: 'https://github.com/mariopedregon/pedregon_view_tool'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,10 @@ class ApplicationController < ActionController::Base
   include SetSource
   include CurrentUserConcern
   include DefaultPageContent
+
+  before_action :set_copyright
+
+   def set_copyright
+  	  @copyright = PedregonViewTool::Renderer.copyright "Mario Pedregon " , "All Rights Resvered"
+   end
 end
